0a7e27374995df14298b04f94dac3500c8ed452f
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2022-10-24 19:14:05 +0900

    Search

--- socialmedia/src/components/profile/ProfileSide.jsx
+++ socialmedia/src/components/profile/ProfileSide.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import LogoSearh from '../LogoSearch/LogoSearh';
 
 const ProfileSide = () => {
   return (
     <div>
       <div className="ProfileSide">
-        ProfileSide
+        <LogoSearh />
       </div>
     </div>
   );
