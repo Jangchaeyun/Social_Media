ce340ea9d6802253decaa08e2c520680e3da3e9a
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2022-10-25 19:26:46 +0900

    Trend Card

---
+++ socialmedia/src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData = [
+    {
+        name: "React",
+        share: 97,
+    },
+    {
+        name: "Frontend",
+        share: 80.5,
+    },
+    {
+        name: "Next.js",
+        share: 75.5,
+    },
+    {
+        name: "Reactjs",
+        share: 72,
+    },
+    {
+        name: "WebDeveloper",
+        share: 71.9,
+    },
+    {
+        name: "Html",
+        share: 20,
+    }
+]
