0f6ed4732df2dcac26ebc9e1ccfd590485f43c25
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2022-10-25 15:40:47 +0900

    Posts

---
+++ socialmedia/src/Data/PostsData.js
@@ -0,0 +1,35 @@
+import postPic1 from '../img/postpic1.png'
+import postPic2 from '../img/postpic2.png'
+import postPic3 from '../img/postpic3.png'
+import postPic4 from '../img/postpic4.png'
+
+export const PostsData = [
+    {
+        img: postPic1,
+        name: 'BH0506',
+        desc: "리액트 공부 중입니다~~~",
+        likes: 506,
+        liked: true
+    },
+    {
+        img: postPic2,
+        name: 'MinSuck0826',
+        desc: "프론트엔드 개발자가 되고 싶습니다.",
+        likes: 826,
+        liked: true,
+    },
+    {
+        img: postPic3,
+        name: "Berry0312",
+        desc: "카페에서 공부 중 ^^",
+        likes: 312,
+        liked: false,
+    },
+    {
+        img: postPic4,
+        name: "sera0915",
+        desc: "취업준비 하기!! 포토폴리오 작성하기!!",
+        likes: 915,
+        liked: true,
+    },
+]
